cd2d89e370e40b25c5f606876b2b5ac532293b18
Otto Bittner <otto-bittner@gmx.de>
2019-12-14 16:48:59 +0100

    Add query template

---
+++ joern-cli/src/test/scala/io/shiftleft/joern/QueryFor.scala
@@ -0,0 +1,21 @@
+package io.shiftleft.joern
+
+import better.files.File
+import org.scalatest.{Matchers, WordSpec}
+
+import io.shiftleft.semanticcpg.language._
+
+/**
+ * Test code that shows how code property graphs can be
+ * generated using the FuzzyC language frontend
+ * */
+class QueryFor extends WordSpec with Matchers with AbstractJoernCliTest {
+
+  "should generate and load CPG for example code" in withTestCpg(
+    File(getClass.getClassLoader.getResource("testcode/free"))) {
+    case (cpg, _) =>
+      // Query to retrieve all method names
+      cpg.method.name.l should not be empty
+  }
+
+}
